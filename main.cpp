ba5e6952dfb5816d77a6cc8fe265270e21ebeeca
Ilia Khidri <s058048@ad.viko.lt>
2024-09-19 11:40:20 +0300

    Exercise #1 is in progress

--- main.cpp
+++ main.cpp
@@ -4,47 +4,37 @@ using namespace std;
 
 int main()
 {
-    int choice = 0;
-    bool isRunning = true;
-    int num1, num2, sum;
+    int studentMark = 0;
+    int sum = 0;
+    int count = 0;
 
-    while (isRunning)
+    while (true)
     {
-        cout << "Paprastas meniu pasirinkimas\n";
-        cout << "[1]: Pirma opcija\n";
-        cout << "[2]: Antra opcija\n";
-        cout << "[3]: Trecia opcija\n";
-        cout << "[4]: Iseiti is programos\n";
+        cout << "Iveskite studento pazymi (1-10; 0 - baigti ivedima):";
+        cin >> studentMark;
 
-        cout << "Iveskite jusu pasirinkta opcija (skaiciu):";
-        cin >> choice;
+        if (studentMark == 0)
+            break;
 
-        switch (choice)
+        if (studentMark < 1 || studentMark > 10)
         {
-        case 1:
-            cout << "Jus pasirinkote opcija #1\n";
-            cout <<"Iveskite pirma skaiciu"<<endl;
-            cin >> num1;
-            cout <<"Iveskite antra skaiciu"<<endl;
-            cin >> num2;
-            sum = num1 + num2;
-            cout <<"Ivestu dvieju skaiciu suma "<<sum <<endl;
-            break;
-        case 2:
-            cout << "Jus pasirinkote opcija #2\n";
-            break;
-        case 3:
-            cout << "Jus pasirinkote opcija #3\n";
-            break;
-        case 4:
-            cout << "Programa baigia savo darba...\n";
-            isRunning = false;
-            break;
-        default:
-            cout << "Neteisingas pasirinkimas. Bandykite is naujo.\n";
-            break;
+            cout << "Studento pazymis turi buti tarp 1 ir 10. Bandykite is naujo.\n";
+            continue;
         }
+
+        sum += studentMark;
+        count++;
     }
+
+    if (count == 0)
+        count = 1;
+
+    float average = static_cast<float>(sum) / static_cast<float>(count);
+
+    cout << "Studento pazymiu vidurkis:" << average << endl;
+
+    if (average >= 5)
+        cout << "Vidurkis teigiamas." << endl;
 
     return 0;
 }
