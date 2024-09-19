b49c4b7743daa7fdbff6a1f7832668134a5711f4
Ilia Khidri <s058048@ad.viko.lt>
2024-09-19 10:45:23 +0300

    Main menu while loop

--- main.cpp
+++ main.cpp
@@ -5,33 +5,38 @@ using namespace std;
 int main()
 {
     int choice = 0;
+    bool isRunning = true;
 
-    cout << "Paprastas meniu pasirinkimas\n";
-    cout << "[1]: Pirma opcija\n";
-    cout << "[2]: Antra opcija\n";
-    cout << "[3]: Trecia opcija\n";
-    cout << "[4]: Iseiti is programos\n";
-
-    cout << "Iveskite jusu pasirinkta opcija (skaiciu):";
-    cin >> choice;
-
-    switch (choice)
+    while (isRunning)
     {
-    case 1:
-        cout << "Jus pasirinkote opcija #1\n";
-        break;
-    case 2:
-        cout << "Jus pasirinkote opcija #2\n";
-        break;
-    case 3:
-        cout << "Jus pasirinkote opcija #3\n";
-        break;
-    case 4:
-        cout << "Programa baigia savo darba...\n";
-        break;
-    default:
-        cout << "Neteisingas pasirinkimas. Bandykite is naujo.\n";
-        break;
+        cout << "Paprastas meniu pasirinkimas\n";
+        cout << "[1]: Pirma opcija\n";
+        cout << "[2]: Antra opcija\n";
+        cout << "[3]: Trecia opcija\n";
+        cout << "[4]: Iseiti is programos\n";
+
+        cout << "Iveskite jusu pasirinkta opcija (skaiciu):";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Jus pasirinkote opcija #1\n";
+            break;
+        case 2:
+            cout << "Jus pasirinkote opcija #2\n";
+            break;
+        case 3:
+            cout << "Jus pasirinkote opcija #3\n";
+            break;
+        case 4:
+            cout << "Programa baigia savo darba...\n";
+            isRunning = false;
+            break;
+        default:
+            cout << "Neteisingas pasirinkimas. Bandykite is naujo.\n";
+            break;
+        }
     }
 
     return 0;
